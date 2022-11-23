8798537f14a81e75366c912a50224d60b447436f
Erik Currin <ehc12345678@gmail.com>
2022-11-22 20:41:04 -0500

    Changes for Terry

--- src/main/java/org/robockets/RobotMap.java
+++ src/main/java/org/robockets/RobotMap.java
@@ -1,19 +1,26 @@
 package org.robockets;
 
+import com.ctre.phoenix.motorcontrol.can.WPI_TalonFX;
 import com.revrobotics.CANSparkMax;
 import com.revrobotics.CANSparkMaxLowLevel;
-import edu.wpi.first.wpilibj.GenericHID;
-import edu.wpi.first.wpilibj.Joystick;
-import edu.wpi.first.wpilibj.XboxController;
+import edu.wpi.first.math.kinematics.DifferentialDriveOdometry;
+import edu.wpi.first.wpilibj.ADXRS450_Gyro;
 import edu.wpi.first.wpilibj.drive.DifferentialDrive;
+import edu.wpi.first.wpilibj.motorcontrol.MotorControllerGroup;
 
 public class RobotMap {
-    
-    public static CANSparkMax left = new CANSparkMax(2, CANSparkMaxLowLevel.MotorType.kBrushless);
-    public static CANSparkMax right = new CANSparkMax(1, CANSparkMaxLowLevel.MotorType.kBrushless);
+    // Drivetrain motors. The Talons already have encoders inside them
+    public static WPI_TalonFX front_left = new WPI_TalonFX(0);
+    public static WPI_TalonFX front_right = new WPI_TalonFX(1);
+    public static WPI_TalonFX back_left = new WPI_TalonFX(2);
+    public static WPI_TalonFX back_right = new WPI_TalonFX(3);
 
-    public static DifferentialDrive m_drive = new DifferentialDrive(left,right);
+    // Drivetrain
+    public static final MotorControllerGroup m_leftMotors = new MotorControllerGroup(front_left,back_left);
+    public static final MotorControllerGroup m_rightMotors = new MotorControllerGroup(front_right,back_right);
 
-    //Joystick
-    public static Joystick joystick = new Joystick(0);
+    public static final DifferentialDrive m_drive = new DifferentialDrive(m_leftMotors,m_rightMotors);
+
+    //GyroScope
+    public static ADXRS450_Gyro gyro = new ADXRS450_Gyro();
 }
