7a302d619ee17bcb0e20eb896378a86dae2d1f20
ehc12345678 <ehc12345678@gmail.com>
2022-12-15 19:34:38 -0500

    Update

--- src/main/java/org/robockets/command/MoveFeetForward.java
+++ src/main/java/org/robockets/command/MoveFeetForward.java
@@ -1,22 +1,17 @@
 package org.robockets.command;
 
-import edu.wpi.first.wpilibj2.command.CommandBase;
-import edu.wpi.first.wpilibj2.command.ParallelRaceGroup;
+import edu.wpi.first.wpilibj2.command.SequentialCommandGroup;
 
-public class MoveFeetForward extends CommandBase {
+public class MoveFeetForward extends SequentialCommandGroup {
     private static final double ratio = 1.2;
-    private ParallelRaceGroup moveCommand;
 
     public MoveFeetForward(double speed, double feet) {
-        moveCommand = new MoveCommand(speed, 0).withTimeout(computeTime(speed, feet));
+        super(
+            new MoveCommand(speed, 0).withTimeout(computeTime(speed, feet))
+        );
     }
 
     public static double computeTime(double speed, double feet) {
         return feet * speed * ratio;
-    }
-
-    @Override
-    public void execute() {
-        moveCommand.execute();
     }
 }
