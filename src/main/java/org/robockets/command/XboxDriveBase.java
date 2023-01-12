62efa946255dc629f3123dce7c19a583dda2663b
Erik Currin <ehc12345678@gmail.com>
2023-01-12 16:21:36 -0500

    Use the new xboxController class

--- src/main/java/org/robockets/command/XboxDriveBase.java
+++ src/main/java/org/robockets/command/XboxDriveBase.java
@@ -1,10 +1,10 @@
 package org.robockets.command;
 
 import edu.wpi.first.wpilibj2.command.CommandBase;
-import org.robockets.controller.XboxControl;
+import edu.wpi.first.wpilibj2.command.button.CommandXboxController;
 
 public abstract class XboxDriveBase extends CommandBase {
-    protected XboxControl xbox = new XboxControl(0);
+    protected CommandXboxController xbox = new CommandXboxController(0);
 
     @Override
     public abstract void execute();
